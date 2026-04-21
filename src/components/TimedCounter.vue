aa11f84e49282a5cbf189dbe030fb3013fe7e511
gabriel roy-lortie <gabriel.roylortie@gmail.com>
2026-04-21 00:01:43 -0400

    feat: editable name

--- src/components/TimedCounter.vue
+++ src/components/TimedCounter.vue
@@ -9,11 +9,13 @@ const SUFFIX_LIST = [
   ['P', 1e15], ['E', 1e18], ['Z', 1e21], ['Y', 1e24], ['R', 1e27], ['Q', 1e30]
 ]
 
+const name = ref('Compteur')
 const count = ref(0)
 const velocity = ref(0)
 const velocitySuffix = ref('')
 
 const editing = ref(false)
+const draftName = ref(name.value)
 const draftInput = ref(0)
 const draftSuffix = ref('')
 
@@ -29,6 +31,7 @@ function velocityInput() {
 }
 
 function applyEdit() {
+  name.value = draftName.value
   const multiplier = SUFFIX_LIST.find(([s]) => s === draftSuffix.value)?.[1] ?? 1
   velocity.value = draftInput.value * multiplier
   velocitySuffix.value = draftSuffix.value
@@ -36,6 +39,9 @@ function applyEdit() {
 }
 
 function cancelEdit() {
+  draftName.value = name.value
+  draftInput.value = count.value
+  draftSuffix.value = velocitySuffix.value
   editing.value = false
 }
 
@@ -56,16 +62,28 @@ onUnmounted(() => clearInterval(timer))
 
 <template>
   <div class="timed-counter">
-    <h1>Timed Counter</h1>
+    <!-- Name -->
+    <div class="name">
+      <div v-if="!editing" class="name-display">
+        <h1>{{ name }}</h1>
+      </div>
+      <div v-if="editing" class="name-edit">
+        <input v-model="draftName" type="text" />
+      </div>
+    </div>
+    
+    <!-- Count -->
     <div class="count">{{ formattedCount }} {{ unit }}</div>
     <div>{{ count }} {{ unit }}</div>
-    <div class="velocity-display">
+
+    <!-- Velocity -->
+    <div v-if="!editing" class="velocity-display">
       <span class="velocity-label">Taux&nbsp;:</span>
       <span class="velocity-value">{{ velocityInput() }}{{ velocitySuffix }} {{ unit }}/s</span>
-      <button v-if="!editing" @click="openEdit"><Pencil :size="18" /></button>
     </div>
+    
     <div v-if="editing" class="velocity-input">
-      <label for="velocity-field">Nouveau taux&nbsp;:</label>
+      <label for="velocity-field">Taux&nbsp;:</label>
       <input
         id="velocity-field"
         v-model="draftInput"
@@ -79,11 +97,12 @@ onUnmounted(() => clearInterval(timer))
         <option v-for="[symbol] in SUFFIX_LIST" :key="symbol" :value="symbol">{{ symbol }}</option>
       </select>
       <span>{{ unit }}/s</span>
-      <button @click="applyEdit"><Check :size="18" /></button>
-      <button @click="cancelEdit"><X :size="18" /></button>
     </div>
     <div class="actions">
-      <button @click="count = 0; velocity = 0">Reset</button>
+      <button v-if="!editing" @click="count = 0; velocity = 0">Reset</button>
+      <button v-if="!editing" @click="openEdit"><Pencil :size="18" /></button>
+      <button v-if="editing" @click="applyEdit"><Check :size="18" /></button>
+      <button v-if="editing" @click="cancelEdit"><X :size="18" /></button>
     </div>
   </div>
 </template>
@@ -125,6 +144,16 @@ onUnmounted(() => clearInterval(timer))
 .velocity-input label {
   font-size: 0.875rem;
   color: #555;
+}
+
+.name-edit input {
+  font-size: 1.25rem;
+  padding: 0.4rem 0.75rem;
+  width: 100%;
+  text-align: center;
+  border: 2px solid #333;
+  border-radius: 6px;
+  outline: none;
 }
 
 .velocity-input input {
